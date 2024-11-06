a647d00180c55cf029b19f3cba77a64c4cc507b1
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-11-06 10:40:30 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Demo from './components/Demo'
+import Hero from './components/hero'
+
+
+function App() {
+
+  return (
+    <main>
+      <div className='main'>
+       <div className='gradient'/>
+      </div>
+
+      <div className='app'>
+        <Hero />
+        <Demo/>
+      </div>
+
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+      <header className="w-full flex justify-center items-center flex-col">
+          <nav className="flex justify-between items-center w-full mb-10 pt-3">
+              <h1 className="font-lexend font-bold text-3xl object-contain w-28">SumSmart</h1>
+              <button
+              type="button"
+                  onClick={() => window.open('https://github.com/Tayyabbutt1133/sumsmart')}
+              className="font-lexend flex items-center justify-center gap-2 black_btn transition duration-300">
+                  <FaGithub/>
+                  Github
+              </button>
+          </nav>
+          <h1 className="head_text font-lexend">
+              Summarize Articles with <br className="max-md:hidden"/>
+              <span className="orange_gradient">SumSmart AI</span>
+          </h1>
+          <h2 className="desc font-lexend">
+          Your AI-powered article summarizer for instant insights. Simply drop a link, and SumSmart AI will extract the key points, giving you a quick, clear summary so you can stay informed effortlessly. Perfect for busy readers and knowledge seekers!
+          </h2>
+    </header>
+  );
+};
+
+export default Hero;
