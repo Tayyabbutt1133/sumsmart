8b14ae6c778c0823cac807f701f38cfab62f147e
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2024-11-07 18:56:44 +0500

    images section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
+import Avatar01 from "../../public/images/avatar-01.jpg";
+import Avatar02 from "../../public/images/avatar-02.jpg";
+import Avatar03 from "../../public/images/avatar-03.jpg";
+import Avatar04 from "../../public/images/avatar-04.jpg";
+import Avatar05 from "../../public/images/avatar-05.jpg";
+import Avatar06 from "../../public/images/avatar-06.jpg";
 
 const Hero = () => {
   return (
@@ -14,6 +20,50 @@ const Hero = () => {
                   Github
               </button>
           </nav>
+          <div className="-mx-0.5 flex justify-center -space-x-3">
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar01}
+                  width={32}
+                  height={32}
+                  alt="Avatar 01"
+                />
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar02}
+                  width={32}
+                  height={32}
+                  alt="Avatar 01"
+                />
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar03}
+                  width={32}
+                  height={32}
+                  alt="Avatar 02"
+                />
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar04}
+                  width={32}
+                  height={32}
+                  alt="Avatar 03"
+                />
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar05}
+                  width={32}
+                  height={32}
+                  alt="Avatar 04"
+                />
+                <img
+                  className="box-content rounded-full border-2 border-gray-50"
+                  src={Avatar06}
+                  width={32}
+                  height={32}
+                  alt="Avatar 05"
+                />
+              </div>
           <h1 className="head_text font-lexend">
               Summarize Articles with <br className="max-md:hidden"/>
               <span className="orange_gradient">SumSmart AI</span>
